37768b644b1548a29f4ba9a5cd8815ce6057d925
Maen Terawasi <maent45@gmail.com>
2015-07-27 23:14:43 +1200

    Created a Todo list app
    Added css, created an input field for text, added modals, and Angular
    templates, and more ;)

--- BlankCordovaApp1/bld/Ripple/Android/Debug/app.js
+++ BlankCordovaApp1/bld/Ripple/Android/Debug/app.js
@@ -6,18 +6,36 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 
-angular.module('app', ['ionic'])
+angular.module('todo', ['ionic'])
 
-.run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        /*if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }*/
-        if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleDefault();
-        }
+.controller('TodoCtrl', function ($scope, $ionicModal) {
+    // No need for testing data anymore
+    $scope.tasks = [];
+
+    // Create and load the Modal
+    $ionicModal.fromTemplateUrl('new-task.html', function (modal) {
+        $scope.taskModal = modal;
+    }, {
+        scope: $scope,
+        animation: 'slide-in-up'
     });
-})
+
+    // Called when the form is submitted
+    $scope.createTask = function (task) {
+        $scope.tasks.push({
+            title: task.title
+        });
+        $scope.taskModal.hide();
+        task.title = "";
+    };
+
+    // Open our new task modal
+    $scope.newTask = function () {
+        $scope.taskModal.show();
+    };
+
+    // Close the new task modal
+    $scope.closeNewTask = function () {
+        $scope.taskModal.hide();
+    };
+});
